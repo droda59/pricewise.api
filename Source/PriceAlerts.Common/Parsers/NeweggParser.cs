5cffb45e0903416d4e87aa62e1a51676e536fe63
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-04-26 14:44:26 -0400

    Added Newegg parser

---
+++ Source/PriceAlerts.Common/Parsers/NeweggParser.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+
+using HtmlAgilityPack;
+
+namespace PriceAlerts.Common.Parsers
+{
+    internal class NeweggParser : BaseParser, IParser
+    {
+        public NeweggParser(IHtmlLoader htmlLoader)
+            : base(htmlLoader, new Uri("https://www.newegg.ca/"))
+        {
+        }
+        
+        protected override string GetTitle(HtmlDocument doc)
+        {
+            var titleNode = doc
+                .GetElementbyId("grpDescrip_h")
+                .SelectSingleNode(".//span");
+
+            var extractedValue = titleNode.InnerText.Replace(Environment.NewLine, string.Empty).Trim();
+
+            return extractedValue;
+        }
+
+        protected override string GetImageUrl(HtmlDocument doc)
+        {
+            var nodeValue = doc.DocumentNode
+                .SelectSingleNode(".//span[@class='mainSlide']")
+                .SelectSingleNode(".//img");
+                
+            var extractedValue = nodeValue.Attributes["src"].Value;
+
+            return extractedValue;
+        }
+
+        protected override decimal GetPrice(HtmlDocument doc)
+        {
+            var priceContent = doc.DocumentNode
+                .SelectSingleNode(".//div[@itemprop='offers']")
+                .SelectSingleNode(".//meta[@itemprop='price']")
+                .Attributes["content"].Value;
+
+            var extractedValue = this.ExtractNumber(priceContent);
+            var decimalValue = Convert.ToDecimal(extractedValue);
+
+            return decimalValue;
+        }
+    }
+}
