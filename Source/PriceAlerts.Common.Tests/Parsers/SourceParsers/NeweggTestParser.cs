0d0d2f5b6b147f36e4fd6d760b6ba36f2ca9b26f
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-06-16 18:21:28 -0400

    Added integration tests for Newegg

---
+++ Source/PriceAlerts.Common.Tests/Parsers/SourceParsers/NeweggTestParser.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+
+using PriceAlerts.Common.Parsers.SourceParsers;
+using PriceAlerts.Common.Tests.Parsers;
+
+namespace PriceAlerts.Common.Tests.Parsers.SourceParsers
+{
+    internal class NeweggTestParser : NeweggParser, ITestParser
+    {
+        public NeweggTestParser(IHtmlLoader htmlLoader)
+            : base(htmlLoader)
+        {
+        }
+
+        public async Task<IEnumerable<Uri>> GetTestProductsUrls()
+        {
+            var productUrls = new List<Uri>();
+
+            var document = await this.LoadDocument(this.Domain);
+            
+            productUrls.AddRange(
+                document.GetElementbyId("ShellShocker")
+                    .SelectSingleNode(".//div[@class='swiper-wrapper]")
+                    .SelectNodes(".//div[contains(@class, 'item-container')]//a")
+                    .Select(x => new Uri(x.Attributes["href"].Value)));
+            
+            productUrls.AddRange(
+                document.GetElementbyId("DailyDeals")
+                    .SelectSingleNode(".//div[@class='swiper-wrapper]")
+                    .SelectNodes(".//div[contains(@class, 'item-container')]//a")
+                    .Select(x => new Uri(x.Attributes["href"].Value)));
+            
+            productUrls.AddRange(
+                document.GetElementbyId("Spotlight")
+                    .SelectSingleNode(".//div[@class='swiper-wrapper]")
+                    .SelectNodes(".//div[contains(@class, 'item-container')]//a")
+                    .Select(x => new Uri(x.Attributes["href"].Value)));
+            
+            productUrls.AddRange(
+                document.GetElementbyId("MoreDeals")
+                    .SelectSingleNode(".//div[@class='swiper-wrapper]")
+                    .SelectNodes(".//div[contains(@class, 'item-container')]//a")
+                    .Select(x => new Uri(x.Attributes["href"].Value)));
+
+            return productUrls;
+        }
+    }
+}
