634c29f29b34ad940157a72b4fb99100fc6771c6
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-08-24 23:50:39 -0400

    Remove comment lol

--- Source/PriceAlerts.Api/LinkManipulators/LegoLinkManipulator.cs
+++ Source/PriceAlerts.Api/LinkManipulators/LegoLinkManipulator.cs
@@ -28,7 +28,6 @@ namespace PriceAlerts.Api.LinkManipulators
             var encodedLink = WebUtility.UrlEncode(originalUrl.AbsoluteUri);
             var deepLink = new Uri($"http://click.linksynergy.com/deeplink?id={Id}&mid={AdvertiserId}&murl={encodedLink}");
 
-// https://click.linksynergy.com/deeplink?id=6/oq6I8N2O0&mid=13923&u1=pricewise&murl=https%3A%2F%2Fshop.lego.com%2Fen-CA%2FLondon-Bus-10258
             return deepLink;
         }
     }
