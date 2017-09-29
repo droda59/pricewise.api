c58920240d50d508c3fa6b54cb9ec8dd20d6ec23
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-09-28 22:16:49 -0400

    Added BsonIgnore attribute

--- Source/PriceAlerts.Common/Models/Deal.cs
+++ Source/PriceAlerts.Common/Models/Deal.cs
@@ -1,4 +1,5 @@
 using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace PriceAlerts.Common.Models
 {
--- Source/PriceAlerts.Common/Models/Settings.cs
+++ Source/PriceAlerts.Common/Models/Settings.cs
@@ -1,3 +1,5 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace PriceAlerts.Common.Models
 {
     [BsonIgnoreExtraElements]
