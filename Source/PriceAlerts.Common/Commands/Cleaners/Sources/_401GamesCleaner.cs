b0ab46e94267190eae8db9ad3580f2e33340d77a
Simon Allie <simon.allie@gsoft.com>
2018-02-02 18:06:58 -0500

    Added 401Games

---
+++ Source/PriceAlerts.Common/Commands/Cleaners/Sources/_401GamesCleaner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using PriceAlerts.Common.Sources;
+
+namespace PriceAlerts.Common.Commands.Cleaners.Sources
+{
+    public class _401GamesCleaner : BaseCleaner, ICleaner
+    {
+        private readonly _401GamesSource _source;
+
+        public _401GamesCleaner(_401GamesSource source)
+        {
+            this._source = source;
+        }
+
+        public override Uri CleanUrl(Uri originalUrl)
+        {
+            var urlWithoutQueryString = new UriBuilder(originalUrl) { Query = string.Empty }.Uri;
+            
+            var newUrl = this._source.ProductExpression.Match(urlWithoutQueryString.ToString()).Value;
+            
+            return new Uri(this._source.Domain, newUrl);
+        }
+    }
+}
