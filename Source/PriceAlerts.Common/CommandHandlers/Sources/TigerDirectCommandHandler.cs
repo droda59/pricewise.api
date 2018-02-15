bfd43fd2545d072e8446b451062d3916739d42bd
Maxime Gaudreault Proulx <swcommander@gmail.com>
2018-02-14 21:15:24 -0500

    Added cj link manipulator

--- Source/PriceAlerts.Common/CommandHandlers/Sources/TigerDirectCommandHandler.cs
+++ Source/PriceAlerts.Common/CommandHandlers/Sources/TigerDirectCommandHandler.cs
@@ -1,5 +1,6 @@
 using PriceAlerts.Common.Commands.Cleaners.Sources;
 using PriceAlerts.Common.Commands.Inspectors.Sources;
+using PriceAlerts.Common.Commands.LinkManipulators;
 using PriceAlerts.Common.Sources;
 
 namespace PriceAlerts.Common.CommandHandlers.Sources
@@ -7,12 +8,14 @@ namespace PriceAlerts.Common.CommandHandlers.Sources
     internal class TigerDirectCommandHandler : CommandHandler
     {
         public TigerDirectCommandHandler(TigerDirectSource source, 
+            CJLinkManipulator manipulator, 
             TigerDirectCleaner cleaner, 
             TigerDirectParser parser)
             : base(source)
         {
             this.Commands.Add(cleaner);
             this.Commands.Add(parser);
+            this.Commands.Add(manipulator);
         }
     }
 }
