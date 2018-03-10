763581ba0d76d44a3760762b56d6bf54e44736a7
Mathy Scott <mathy.scott@gsoft.com>
2018-03-10 12:06:38 -0500

    fix sportium tests issues caused by sportium.

--- Source/PriceAlerts.Common.Tests/Parsers/SourceParsers/SportiumTestParser.cs
+++ Source/PriceAlerts.Common.Tests/Parsers/SourceParsers/SportiumTestParser.cs
@@ -22,14 +22,18 @@ namespace PriceAlerts.Common.Tests.Parsers.SourceParsers
         {
             var document = await this._documentLoader.LoadDocument(this.Source.Domain, this.Source.CustomHeaders);
             
-            var urls = document.DocumentNode
-                .SelectSingleNode(".//div[@class='after-content-container']")
-                .SelectNodes(".//div[@class='productbox-banner']")
-                .SelectMany(x => x.SelectNodes("a"))
-                .Select(x => x.Attributes["href"].Value)
-                .Select(x => new Uri(this.Source.Domain, x));
+            var documentNode = document.DocumentNode;
+            var banner = documentNode.SelectNodes(".//ul[@class='products-grid with-carousel']");
+            var links = banner.SelectMany(x => x.SelectNodes(".//a"));
+            var hrefs = links.Select(x => x.Attributes["href"].Value);
+            var urls = hrefs.Select(x => new Uri(this.Source.Domain, x));
 
-            return urls;
+            // At the moment this product page is not working on Sportium's website
+            return urls.Where(
+                x => !x.AbsoluteUri
+                        .Contains("https://www.sportium.ca/en/ccm-epaulieres-6052-jr-251119-3661200002") && 
+                     !x.AbsoluteUri
+                        .Contains("https://www.sportium.ca/en/under-armour-chandail-hg-compression-pour-homme-129636"));
         }
     }
 }
